914af86784eecc6b7b0fbafe207d58ee9f0a8634
Alaoui-Fatimaezzahra <abdelazizalaoui1975@gmail.com>
2023-01-14 13:24:39 +0000

    verify changes

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -4,22 +4,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BubbleSort implements Strategy{
-    public int[] sort (int[] numbers){
-        int j = numbers.length - 1;
+    public int[] sort (int[] nombres){
+        int j = nombres.length - 1;
         int s ;
         boolean  sort = true;
         while (sort && j>0) {
             sort = false;
             for ( int k = 0; k<j; k++){
-                if (numbers[k]>numbers[k+1]){
-                    s= numbers[k];
-                    numbers[k] = numbers[k+1];
-                    numbers[k+1] = s;
+                if (nombres[k]>nombres[k+1]){
+                    s= nombres[k];
+                    nombres[k] = nombres[k+1];
+                    nombres[k+1] = s;
                     sort = true;
                 }
             }
             j = j-1;
         }
-        return numbers;
+        return nombres;
     }
 }
